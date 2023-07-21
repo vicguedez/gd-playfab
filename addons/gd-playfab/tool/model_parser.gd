74ed71d19bb4a23183d18259f1f9c56bb364464c
Victor Guedez <vicguedeztello@gmail.com>
2023-07-21 11:47:21 -0500

    Added extend PlayFabHttpRequest to model parser

--- addons/gd-playfab/tool/model_parser.gd
+++ addons/gd-playfab/tool/model_parser.gd
@@ -6,6 +6,12 @@ const LINES_TO_IGNORE = [
 	"Name 	Required 	Type 	Description",
 	]
 
+enum Extend {
+	STANDALONE,
+	PLAYFABMODEL,
+	PLAYFABHTTPREQUEST
+}
+
 @onready
 var input: TextEdit = $container/input
 @onready
@@ -15,7 +21,7 @@ var output: TextEdit = $container/output
 var input_clear: Button = $container/input_h_container/clear
 
 @onready
-var output_extend: CheckBox = $container/output_h_container/extend
+var output_extend: OptionButton = $container/output_h_container/extend
 @onready
 var output_copy: Button = $container/output_h_container/copy
 
@@ -23,14 +29,14 @@ func _ready() -> void:
 	input.text_changed.connect(_on_input_text_changed)
 	input_clear.pressed.connect(_on_input_clear_pressed)
 	
-	output_extend.toggled.connect(_on_output_extend_toggled)
+	output_extend.item_selected.connect(_on_output_extend_item_selected)
 	output_copy.pressed.connect(_on_output_copy_pressed)
 
 func _on_input_clear_pressed() -> void:
 	input.clear()
 	output.clear()
 
-func _on_output_extend_toggled(_toggled: bool) -> void:
+func _on_output_extend_item_selected(_index: int) -> void:
 	_on_input_text_changed()
 
 func _on_output_copy_pressed() -> void:
@@ -54,10 +60,25 @@ func _on_input_text_changed() -> void:
 	var lines: PackedStringArray = input_text.split("\n", false)
 	var first_line = true;
 	
+	var _extend_model = false
+	var _extend_httprequest = false
+	var _extends = ""
+	var _fields = []
+	var _required_fields = []
+	
+	match output_extend.get_selected_id():
+		Extend.PLAYFABMODEL:
+			_extend_model = true
+			_extends = " extends PlayFabModel"
+		Extend.PLAYFABHTTPREQUEST:
+			_extend_httprequest = true
+			_extends = " extends PlayFabHttpRequest"
+		_:
+			pass
+	
 	while lines.size():
 		if first_line:
-			var _extend = " extends PlayFabModel" if output_extend.button_pressed else ""
-			var _class = "class %s%s:\n" % [lines[0], _extend]
+			var _class = "class %s%s:\n" % [lines[0], _extends]
 			
 			lines.remove_at(0)
 			
@@ -82,13 +103,20 @@ func _on_input_text_changed() -> void:
 		lines.remove_at(0)
 		
 		var line_parts: PackedStringArray = next_line.strip_edges().split("\t", false)
+		var key = line_parts[0].to_snake_case().trim_suffix("_")
+		
+		_fields.append(key)
 		
 		if line_parts.size() > 3:
+			if line_parts[1] == "True":
+				_required_fields.append(key)
+			
 			line_parts.remove_at(1)
 		
-		var key = line_parts[0].to_snake_case()
 		var type = line_parts[1]
+		var type_initialize = false
 		var description = line_parts[2] if line_parts.size() > 2 else ""
+		var initialize = ""
 		
 		if type.begins_with("string"):
 			type = type.replace("string", "String")
@@ -96,10 +124,16 @@ func _on_input_text_changed() -> void:
 			type = type.replace("boolean", "bool")
 		elif type.begins_with("object"):
 			type = type.replace("object", "Dictionary")
+			type_initialize = true
 		elif type.begins_with("number"):
 			type = type.replace("number", "float")
 		elif type.begins_with("integer"):
 			type = type.replace("integer", "int")
+		else:
+			if _extend_httprequest:
+				type = "PlayFabModel.%s" % type
+			
+			type_initialize = true
 		
 		if type.ends_with("[]"):
 			type = "Array[%s]" % type.substr(0, type.length() - 2)
@@ -110,6 +144,30 @@ func _on_input_text_changed() -> void:
 			
 			output.insert_text_at_caret("\t## %s\n" % description)
 		
-		output.insert_text_at_caret("\tvar %s: %s\n" % [key, type])
+		if (
+				_extend_httprequest
+				and type_initialize
+			):
+			
+			if type.begins_with("Array["):
+				initialize = " = []"
+			elif type == "Dictionary":
+				initialize = " = {}"
+			else:
+				initialize = " = %s.new()" % type
+		
+		output.insert_text_at_caret("\tvar %s: %s%s\n" % [key, type, initialize])
+	
+	if (_extend_httprequest):
+		output.insert_text_at_caret("\t\n\tfunc get_config() -> Dictionary:\n")
+		output.insert_text_at_caret("\t\treturn {\n")
+		output.insert_text_at_caret('\t\t\t"fields": {\n')
+		
+		for field in _fields:
+			output.insert_text_at_caret('\t\t\t\t"%s": %s,\n' % [field, field])
+		
+		output.insert_text_at_caret('\t\t\t},\n')
+		output.insert_text_at_caret('\t\t\t"required_fields": %s,\n' % JSON.stringify(_required_fields))
+		output.insert_text_at_caret("\t\t}\n")
 	
 	output.insert_text_at_caret("\n")
