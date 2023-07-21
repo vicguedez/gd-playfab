7d9fa4346f9d5f793f1d24f71444e61f744fd5d7
Victor Guedez <vicguedeztello@gmail.com>
2023-07-21 08:11:16 -0500

    Renamed tool_model_parser to model_parser

--- addons/gd-playfab/plugin.gd
+++ addons/gd-playfab/plugin.gd
@@ -6,16 +6,16 @@ const SINGLETON_PATH := "res://addons/gd-playfab/singleton/playfab.tscn"
 
 const MODEL_PARSER = preload("res://addons/gd-playfab/tool/model_parser.tscn")
 
-var tool_model_parser: Control
+var model_parser: Control
 
 func _enter_tree() -> void:
-	tool_model_parser = MODEL_PARSER.instantiate()
+	model_parser = MODEL_PARSER.instantiate()
 	
 	add_autoload_singleton(SINGLETON_NAME, SINGLETON_PATH)
-	add_control_to_bottom_panel(tool_model_parser, "PlayFab Model Parser")
+	add_control_to_bottom_panel(model_parser, "PlayFab Model Parser")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton(SINGLETON_NAME)
-	remove_control_from_bottom_panel(tool_model_parser)
+	remove_control_from_bottom_panel(model_parser)
 	
-	tool_model_parser.queue_free()
+	model_parser.queue_free()
