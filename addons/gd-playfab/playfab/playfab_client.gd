d74322144a348438f473c1e91bd72b9e7688e4af
Victor Guedez <vicguedeztello@gmail.com>
2023-07-28 10:18:28 -0500

    PlayFabClient extend PlayFabHttpRequest

--- addons/gd-playfab/playfab/playfab_client.gd
+++ addons/gd-playfab/playfab/playfab_client.gd
@@ -1,9 +1,9 @@
 ## SDK for PlayFab client management and services.
-extends RefCounted
+extends PlayFabHttpRequest
 class_name PlayFabClient
 
 ## Signs the user into the PlayFab account, returning a session identifier that can subsequently be used for API calls which require an authenticated user. Unlike most other login API calls, LoginWithEmailAddress does not permit the creation of new accounts via the CreateAccountFlag. Email addresses may be used to create accounts via RegisterPlayFabUser.
-class LoginWithEmailAddress extends PlayFabHttpRequest:
+class LoginWithEmailAddress extends PlayFabClient:
 	## Email address for the account.
 	var email: String
 	## Password for the PlayFab account (6-100 characters).
